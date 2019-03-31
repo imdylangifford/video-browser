d37dfe12d45671cf6cc7edbf49a986a27e0e2c30
imdylangifford <imdylangifford@gmail.com>
2019-03-31 15:17:19 -0400

    configured search events

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <SearchBar @termChange="onTermChange"></SearchBar>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from './components/SearchBar.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        SearchBar
+    },
+    methods: {
+        onTermChange(searchTerm) {
+            console.log(searchTerm)
+        }
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
